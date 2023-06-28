8cb36cdb526ebaa5c7e10e8deea3cc190825b895
chaamu <125999531+chaamu@users.noreply.github.com>
2023-06-28 18:16:06 +0530

    Add files via upload

---
+++ string-conversion.py
@@ -0,0 +1,15 @@
+str1="python"
+str2=str1.upper()
+print(f'{str1}--->{str2}')
+str3="python"
+str4=str3.lower()
+print(f'{str3}--->{str4}')
+str5="python programing language"
+str6=str5.title()
+print(f'{str5}---> {str6}')
+str7="PYTHON PROGRAMING"
+str8=str7.capitalize()
+print(f'{str7}--->{str8}')
+str9="pYTHON"
+str10=str9.swapcase()
+print(f'{str9}--->{str10}')
