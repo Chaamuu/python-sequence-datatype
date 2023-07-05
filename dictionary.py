13a6747cdff4f5d64458c84ad53706ada6909e55
chaamu <125999531+chaamu@users.noreply.github.com>
2023-07-05 06:06:57 -0700

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+players={'dhoni':40,
+        'rahul':30,
+        'geeta':70}
+        
+print(players)
+s1=players['dhoni']
+s2=players['rahul']
+s3=players['geeta']
+print(s1,s2,s3)
+print()
+
+sales={2000:45000,2001:5000,2002:5002,2003:5600}
+print(sales)
+s1=sales[2002]
+s2=sales[2000]
+print(s1,s2)
