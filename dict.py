407b36d0f240d40d61f31f2e0f813702a3990d7d
chaamu <125999531+chaamu@users.noreply.github.com>
2023-07-05 06:40:56 -0700

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,11 @@
+grade_dict={'naresh':'A',
+            'raji':'B',
+            'pushpa':'c'}
+            
+for name in grade_dict:
+    print(name,grade_dict[name])
+    
+    
+d1={1:'one',2:'two',3:'there'}
+for k in d1:
+    print(k,d1[k])
