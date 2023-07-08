369bbc18037d4bf6bceb0a33ca217010bfd124bc
chaamu <125999531+chaamu@users.noreply.github.com>
2023-07-08 06:14:10 -0700

    Create func.py

---
+++ func.py
@@ -0,0 +1,18 @@
+def draw_line():
+    for i in range(20):
+        print("*",end='')
+    print()
+
+def func1(a):
+    print(a)
+#main program
+
+draw_line()
+print("python")
+draw_line()
+print("language")
+draw_line()
+
+draw_line(10)
+
+func1(10)
